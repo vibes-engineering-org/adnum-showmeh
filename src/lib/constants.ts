908f7f1d1ca2b2f15aa2f7696aff2582b7b32b80
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-13 11:11:10 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "showmeh";
+export const PROJECT_DESCRIPTION = "by @adnum";
+export const PROJECT_CREATOR = "adnum";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
